1ed72cfd89c85928f3bc85c1383f96eb61d5f6c7
Sanchez <atos.holdemgk@gmail.com>
2020-10-25 16:01:33 +0300

    Update lb-content-tf.sh

--- labs/lb-content-tf.sh
+++ labs/lb-content-tf.sh
@@ -1,1 +1,9 @@
 source envs.sh
+terraform version
+git clone https://github.com/GoogleCloudPlatform/terraform-google-lb-http.git
+cd ~/terraform-google-lb-http/examples/multi-backend-multi-mig-bucket-https-lb
+terraform init
+terraform plan --out=tfplan 'project=${PROJECT}'
+terraform apply tfplan
+EXTERNAL_IP=$(terraform output | grep load-balancer-ip | cut -d = -f2 | xargs echo -n)
+echo https://${EXTERNAL_IP}

258af8bb1ad261d42327e2d6095ecb6b04b03bd3
Sanchez <atos.holdemgk@gmail.com>
2020-10-29 17:24:44 +0300

    Update lb-modular-region.sh

--- labs/lb-modular-region.sh
+++ labs/lb-modular-region.sh
@@ -6,3 +6,13 @@ tfswitch -b ${HOME}/bin/terraform 0.11.14
 echo "0.11.14" >> .tfswitchrc
 exit
 terraform version
+git clone https://github.com/GoogleCloudPlatform/terraform-google-lb
+cd ~/terraform-google-lb/
+git checkout 028dd3dc09ae1bf47e107ab435310c0a57b1674c
+cd ~/terraform-google-lb/examples/basic
+export PROJECT=$(gcloud config get-value project)
+terraform init
+terraform plan
+terraform apply
+EXTERNAL_IP=$(terraform output | grep load-balancer-ip | cut -d = -f2 | xargs echo -n)
+echo "http://${EXTERNAL_IP}"

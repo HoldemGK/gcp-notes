d17287fb8a5eaaabe7d15838e4a53757d7d913d4
Sanchez <atos.holdemgk@gmail.com>
2020-11-20 13:54:05 +0300

    Create steps.sh

---
+++ tf-sql/steps.sh
@@ -0,0 +1,23 @@
+# Cloud SQL with Terraform
+
+export PROJECT=$(gcloud config get-value project)
+export ZONE=us-central1
+terraform version
+
+git clone https://github.com/HoldemGK/gcp-notes.git
+cd gcp-notes/tf-sql
+
+terraform init
+terraform plan -out=tfplan
+terraform apply tfplan
+
+# Installing the Cloud SQL Proxy
+wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O cloud_sql_proxy
+chmod +x cloud_sql_proxy
+
+DB_NAME=$(terraform output -json | jq -r '.instance_name.value')
+CONN_NAME="${PROJECT}:${ZONE}:${DB_NAME}"
+
+./cloud_sql_proxy -instances=${CONN_NAME}=tcp:3306
+
+echo PASSWORD=$(terraform output -json | jq -r '.generated_user_password.value')

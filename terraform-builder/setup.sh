9f20613f83165d37a86ec51061d03a2ad9725f68
Sanchez <atos.holdemgk@gmail.com>
2021-05-04 17:47:37 +0300

    Update setup.sh

--- terraform-builder/setup.sh
+++ terraform-builder/setup.sh
@@ -12,4 +12,14 @@ gcloud app create --region=$REGION
 gcloud source repos create ${REPO_NAME}
 git config --global credential.https://sourcedevelopers.google.com.helper gcloud.sh
 git remote add google https://source.developers.google.com/p/${PROJECT_ID}/r/${REPO_NAME}
-git push google main
+git push google master
+
+#create the build trigger in cloud build
+gcloud beta builds triggers create cloud-source-repositoies \
+  --build-config=cloudbuild.yaml --repo=${REPO_NAME} \
+  --branch-pattern=^master$ --description="terraform-builder-trigger"
+
+#disable the trigger, we will run it from cloud functions
+gcloud beta builds triggers export terraform-builder-trigger --destination=../cloudbuilder.yaml
+echo disabled: True >> ../cloudbuilder.yaml
+gcloud beta builds triggers import --source=../cloudbuilder.yaml

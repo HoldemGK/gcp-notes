4a52c43005a76b9909caf54efda6145f00dc3425
Sanchez <atos.holdemgk@gmail.com>
2021-05-05 18:32:12 +0300

    Update main.py

--- terraform-builder/cloud-function/main.py
+++ terraform-builder/cloud-function/main.py
@@ -3,7 +3,7 @@ import os
 import traceback
 
 def trigger_build(data, context):
-    from google.cloud.devtools import cloudbuild_vq
+    from google.cloud.devtools import cloudbuild_v1
     client = cloudbuild.CloudBuildClient()
     project_id = os.environ['PROJECT_ID']
     trigger_id = 'terraform-builder-trigger'

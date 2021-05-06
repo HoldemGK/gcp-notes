f594b5cfd91f7f905536425ce385168c111c5aeb
Sanchez <atos.holdemgk@gmail.com>
2021-05-06 12:05:23 +0300

    debug

--- terraform-builder/cloud-function/main.py
+++ terraform-builder/cloud-function/main.py
@@ -8,6 +8,7 @@ def trigger_build(data, context):
     project_id = os.environ['PROJECT_ID']
     trigger_id = 'terraform-builder-trigger'
     source = {"project_id": project_id, "branch_name": "master"}
+    traceback.print_tb(project_id, source)
     try:
         response = client.run_build_trigger(project_id=project_id, trigger_id=trigger_id, source=source)
         print("Called Trigger")

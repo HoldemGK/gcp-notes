fa067a5bb89dfb896dc4f70235e99d25ab68c434
Sanchez <atos.holdemgk@gmail.com>
2021-05-06 14:57:11 +0300

    Update test.py

--- terraform-builder/cloud-function/test.py
+++ terraform-builder/cloud-function/test.py
@@ -5,7 +5,7 @@ def trigger_build(event, context):
 
     project_id = os.getenv('PROJECT_ID')
     trigger_id = 'terraform-builder-trigger'
-    source = {'branch_name': 'master'}
+    source = {'project_id': project_id, 'branch_name': 'master'}
     response = client.run_build_trigger(project_id, trigger_id, source)
 
 if __name__=="__main__":

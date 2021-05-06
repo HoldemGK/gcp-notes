846c0a175165662bad1b19a2c5ea6b120d4271f3
Sanchez <atos.holdemgk@gmail.com>
2021-05-06 15:05:54 +0300

    Update test.py

--- terraform-builder/cloud-function/test.py
+++ terraform-builder/cloud-function/test.py
@@ -3,10 +3,11 @@ def trigger_build(event, context):
     import os
     client = cloudbuild_v1.CloudBuildClient()
 
-    project_id = os.getenv('PROJECT_ID')
-    trigger_id = 'terraform-builder-trigger'
-    source = {'project_id': project_id, 'branch_name': 'master'}
-    response = client.run_build_trigger(project_id, trigger_id, source)
+    source = {"project_id": "test-tf-bulder", "branch_name": "master"}
+    response = client.run_build_trigger(
+      project_id = "test-tf-bulder",
+      trigger_id = "terraform-builder-trigger",
+      source = source)
 
 if __name__=="__main__":
     trigger_build(event=None,context=None)

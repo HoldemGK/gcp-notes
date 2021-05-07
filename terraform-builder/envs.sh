3495a18a5ad6b51e0e4e80c76e3321ab482bc8a4
Sanchez <atos.holdemgk@gmail.com>
2021-05-07 10:39:17 +0300

    finish

--- terraform-builder/envs.sh
+++ terraform-builder/envs.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-# ---  Validate environment variables - Tick/OK, Cross/Not OK
-function statusCheck(){
-  if [ -z "$1" ]
-  then
-    printf "\u274c $2\n"
-  else
-    printf "\u2714 $2\n"
-  fi
-}
 
 export REGION=us-east1
 export ZONE=${REGION}-b
@@ -17,6 +8,20 @@ export REPO_NAME=terraform-builder
 export SENDER=$(gcloud info --format='value(config.account)')
 export RECIPIENT=$(gcloud info --format='value(config.account)')
 export REPO_LINK='https://source.developers.google.com'
+
+# Update the compute environment
+gcloud config set compute/region $GCP_REGION
+gcloud config set compute/zone  $GCP_ZONE
+
+# ---  Validate environment variables - Tick/OK, Cross/Not OK
+function statusCheck(){
+  if [ -z "$1" ]
+  then
+    printf "\u274c $2\n"
+  else
+    printf "\u2714 $2\n"
+  fi
+}
 
 # Call the function statusCheck to validate the environment
 statusCheck "$REGION" "REGION"

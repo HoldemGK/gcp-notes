d4cb574438c9b964476f1e41bc140f0e6db9b74a
Sanchez <atos.holdemgk@gmail.com>
2021-05-20 12:53:48 +0300

    tf-networks

---
+++ tf-networks/vpc-firewall-rules-private.tf
@@ -0,0 +1,39 @@
+# allow ssh
+resource "google_compute_firewall" "private-allow-ssh" {
+  name    = "${google_compute_network.private-vpc.name}-allow-ssh"
+  network = google_compute_network.private-vpc.name
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  source_ranges = [
+    var.subnet_cidr_private
+  ]
+  target_tags = ["allow-ssh"]
+}
+
+# allow rdp
+resource "google_compute_firewall" "private-allow-rdp" {
+  name    = "${google_compute_network.private-vpc.name}-allow-rdp"
+  network = google_compute_network.private-vpc.name
+  allow {
+    protocol = "tcp"
+    ports    = ["3389"]
+  }
+  source_ranges = [
+    var.subnet_cidr_private
+  ]
+  target_tags = ["allow-rdp"]
+}
+
+# allow ping only from everywhere
+resource "google_compute_firewall" "private-allow-ping" {
+  name    = "${google_compute_network.private-vpc.name}-allow-ping"
+  network = google_compute_network.private-vpc.name
+  allow {
+    protocol = "icmp"
+  }
+  source_ranges = [
+    var.subnet_cidr_private
+  ]
+}
---
+++ tf-networks/vpc-network-private.tf
@@ -0,0 +1,12 @@
+resource "google_compute_network" "private-vpc" {
+  name                    = "private-vpc"
+  auto_create_subnetworks = "false"
+  routing_mode            = "GLOBAL"
+}
+
+resource "google_compute_subnetwork" "private-subnet_1" {
+  name          = "private-subnet-1"
+  ip_cidr_range = var.subnet_cidr_private
+  network       = google_compute_network.private-vpc.name
+  region        = var.gcp_region_1
+}

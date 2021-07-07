415e422e0e386d133fbeb4f269ae9a903f8aea12
Sanchez <atos.holdemgk@gmail.com>
2021-07-07 14:46:22 +0300

    added firewall

--- pulumi-server/gcp-python/__main__.py
+++ pulumi-server/gcp-python/__main__.py
@@ -8,17 +8,28 @@ instance_name = config.require('instance_name')
 instance_type = config.require('instance_type')
 instance_image = config.require('instance_image')
 instance_disk_size = config.require('instance_disk_size')
+network_tags = config.require('network_tags')
+firewall_name = config.require('firewall_name')
 
+firewall = compute.Firewall(firewall_name,
+  network = "default",
+  allows = [
+        gcp.compute.FirewallAllowArgs(
+            protocol="udp",
+            ports=["2456-2457"]),
+        gcp.compute.FirewallAllowArgs(
+            protocol="tcp",
+            ports=["2456-2457"]
+        )],
+  source_ranges = ["0.0.0.0/0"])
 
 instance = gcp.compute.Instance(instance_name,
     machine_type=instance_type,
     zone=instance_zone,
-    tags=[
-        "steam-server"
-    ],
+    tags=network_tags,
     boot_disk=gcp.compute.InstanceBootDiskArgs(
         initialize_params=gcp.compute.InstanceBootDiskInitializeParamsArgs(
-            image="ubuntu-os-cloud/ubuntu-2004-lts",
+            image=instance_image,
         ),
     ),
     network_interfaces=[gcp.compute.InstanceNetworkInterfaceArgs(

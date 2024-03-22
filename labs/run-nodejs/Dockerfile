a0bf103848f59ee7db476f9cfdfe283ee34d0bda
HoldemGK <atos.holdemgk@gmail.com>
2024-03-22 17:30:13 +0100

    added cloud run node js lab

---
+++ labs/run-nodejs/Dockerfile
@@ -0,0 +1,9 @@
+FROM node:20
+RUN apt-get update -y \
+    && apt-get install -y libreoffice \
+    && apt-get clean
+WORKDIR /usr/src/app
+COPY package.json package*.json ./
+RUN npm install --only=production
+COPY . .
+CMD [ "npm", "start" ]
---
+++ labs/run-nodejs/prep.sh
@@ -0,0 +1,58 @@
+export REGION=europe-north1
+git clone https://github.com/rosera/pet-theory.git
+cd pet-theory/lab03
+cat package.json
+npm install express
+npm install body-parser
+npm install child_process
+npm install @google-cloud/storage
+gcloud builds submit --tag gcr.io/$GOOGLE_CLOUD_PROJECT/pdf-converter
+
+gcloud run deploy pdf-converter \
+  --image gcr.io/$GOOGLE_CLOUD_PROJECT/pdf-converter \
+  --platform managed \
+  --region Region \
+  --no-allow-unauthenticated \
+  --max-instances=1
+
+SERVICE_URL=$(gcloud beta run services describe pdf-converter --platform managed --region $REGION --format="value(status.url)")
+echo $SERVICE_URL
+
+curl -X POST -H "Authorization: Bearer $(gcloud auth print-identity-token)" $SERVICE_URL
+
+gsutil mb gs://$GOOGLE_CLOUD_PROJECT-upload
+gsutil mb gs://$GOOGLE_CLOUD_PROJECT-processed
+
+gsutil notification create -t new-doc -f json -e OBJECT_FINALIZE gs://$GOOGLE_CLOUD_PROJECT-upload
+
+gcloud iam service-accounts create pubsub-cloud-run-invoker --display-name "PubSub Cloud Run Invoker"
+gcloud beta run services add-iam-policy-binding pdf-converter \
+  --member=serviceAccount:pubsub-cloud-run-invoker@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com \
+  --role=roles/run.invoker --platform managed --region $REGION
+
+gcloud projects list
+export PROJECT_NUMBER=123456789
+
+gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT \
+  --member=serviceAccount:service-$PROJECT_NUMBER@gcp-sa-pubsub.iam.gserviceaccount.com \
+  --role=roles/iam.serviceAccountTokenCreator
+
+gcloud beta pubsub subscriptions create pdf-conv-sub --topic new-doc \
+  --push-endpoint=$SERVICE_URL \
+  --push-auth-service-account=pubsub-cloud-run-invoker@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com
+
+gsutil -m cp gs://spls/gsp644/* gs://$GOOGLE_CLOUD_PROJECT-upload
+
+gsutil -m rm gs://$GOOGLE_CLOUD_PROJECT-upload/*
+
+gcloud run deploy pdf-converter \
+  --image gcr.io/$GOOGLE_CLOUD_PROJECT/pdf-converter \
+  --platform managed \
+  --region $REGION \
+  --memory=2Gi \
+  --no-allow-unauthenticated \
+  --max-instances=1 \
+  --set-env-vars PDF_BUCKET=$GOOGLE_CLOUD_PROJECT-processed
+
+curl -X POST -H "Authorization: Bearer $(gcloud auth print-identity-token)" $SERVICE_URL
+gsutil -m cp gs://spls/gsp644/* gs://$GOOGLE_CLOUD_PROJECT-upload
